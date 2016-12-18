ccf2ae04dedd0dc5a9b51c65778fca42e99ca0b4
Omar EL BALLOUTI <omar.elballouti@std.ecc>
2016-12-18 19:49:33 +0000

    profile premisses

--- MyApplication10/app/src/main/java/com/example/ghita/myapplication10/HomePage2.java
+++ MyApplication10/app/src/main/java/com/example/ghita/myapplication10/HomePage2.java
@@ -5,6 +5,7 @@ import android.app.FragmentTransaction;
 import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,12 +13,15 @@ import android.widget.Button;
 
 public class HomePage2 extends AppCompatActivity {
 
+    private Toolbar toolbar;
     boolean status = false;
     Button btn1,btn2;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.containers2);
+        toolbar = (Toolbar)findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
         btn1 = (Button)findViewById(R.id.profil);
         btn2 = (Button)findViewById(R.id.schedul);
         FragmentManager fm = getFragmentManager();
