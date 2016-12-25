83ca3ed7424f259c1a0706b7735ed1ae617e4fc9
Omar EL BALLOUTI <omar.elballouti@std.ecc>
2016-12-25 20:26:57 +0000

    Some changes on the profile fragment

--- MyApplication10/app/src/main/java/com/example/ghita/myapplication10/Profile.java
+++ MyApplication10/app/src/main/java/com/example/ghita/myapplication10/Profile.java
@@ -1,6 +1,7 @@
 package com.example.ghita.myapplication10;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import static android.view.View.*;
 
@@ -17,11 +19,16 @@ public class Profile extends Fragment{
     public Profile(){
 
     }
+
+    Context ctx;
+    ImageView img;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.profile_view2, container, false);
+        ImageView imageView = (ImageView) v.findViewById(R.id.imageView4);
         //Button button = (Button)v.findViewById(R.id.schedul);
         //button.setOnClickListener(this);
         return v;
